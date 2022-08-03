5348ac571d362f0a77b091055b795d202e7c9152
FeudjioBrand <brandonfeudjio8@gmail.com>
2022-08-03 20:16:54 +0200

    version 1.0.2

---
+++ src/components/Crocs.js
@@ -0,0 +1,172 @@
+import React from 'react';
+
+const crocs = () => {
+    return (
+        <div>
+             <div className="container">
+
+<h3 className="title">QUELQUES PRODUITS</h3>          
+
+<div className="products-container">
+
+   <div className="product" data-name="p-1">
+      <img src="img/bed.jpg" alt=""></img>
+      <h3>Produit</h3>
+      <div className="price">Prix</div>
+   </div>
+
+   <div className="product" data-name="p-2">
+      <img src="" alt=""></img>
+      <h3>Produit</h3>
+      <div className="price">Prix</div>
+   </div>
+
+   <div className="product" data-name="p-3">
+      <img src="" alt=""></img>
+      <h3>Produit</h3>
+      <div className="price">Prix</div>
+   </div>
+
+   <div className="product" data-name="p-4">
+      <img src="" alt=""></img>
+      <h3>Produit</h3>
+      <div className="price">Prix</div>
+   </div>
+
+   <div className="product" data-name="p-5">
+      <img src="" alt=""></img>
+      <h3>Produit</h3>
+      <div className="price">Prix</div>
+   </div>
+
+   <div className="product" data-name="p-6">
+      <img src="" alt=""></img>
+      <h3>Produit</h3>
+      <div className="price">Prix</div>
+   </div>
+
+</div>
+
+</div>
+<div className="products-preview">
+
+<div className="preview" data-target="p-1">
+   <i className="fas fa-times"></i>
+   <img src="" alt=""></img>
+   <h3>Produit</h3>
+   <div className="stars">
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star-half-alt"></i>
+   </div>
+   <p>Details du produit</p>
+   <div className="price">Prix</div>
+   <div className="buttons">
+      <a href="#" className="buy">COMMANDEZ</a>
+      <a href="#" className="cart">Ajoutez au Panier</a>
+   </div>
+</div>
+
+<div className="preview" data-target="p-2">
+   <i className="fas fa-times"></i>
+   <img src="" alt=""></img>
+   <h3>Produit</h3>
+   <div className="stars">
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star-half-alt"></i>
+   </div>
+   <p>Details du produit</p>
+   <div className="price">Prix</div>
+   <div className="buttons">
+      <a href="#" className="buy">COMMANDEZ</a>
+      <a href="#" className="cart">Ajoutez au Panier</a>
+   </div>
+</div>
+
+<div className="preview" data-target="p-3">
+   <i className="fas fa-times"></i>
+   <img src="" alt=""></img>
+   <h3>Produit</h3>
+   <div className="stars">
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star-half-alt"></i>
+   </div>
+   <p>Details du produit</p>
+   <div className="price">Prix</div>
+   <div className="buttons">
+      <a href="#" className="buy">COMMANDEZ</a>
+      <a href="#" className="cart">Ajoutez au Panier</a>
+   </div>
+</div>
+
+<div className="preview" data-target="p-4">
+   <i className="fas fa-times"></i>
+   <img src="" alt=""></img>
+   <h3>Produit</h3>
+   <div className="stars">
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star-half-alt"></i>
+   </div>
+   <p>Details du produit</p>
+   <div className="price">Prix</div>
+   <div className="buttons">
+      <a href="#" className="buy">COMMANDEZ</a>
+      <a href="#" className="cart">Ajoutez au Panier</a>
+   </div>
+</div>
+
+<div className="preview" data-target="p-5">
+   <i className="fas fa-times"></i>
+   <img src="" alt=""></img>
+   <h3>Produit</h3>
+   <div className="stars">
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star-half-alt"></i>
+   </div>
+   <p>Details du produit</p>
+   <div className="price">Prix</div>
+   <div className="buttons">
+      <a href="#" className="buy">COMMANDEZ</a>
+      <a href="#" className="cart">Ajoutez au Panier</a>
+   </div>
+</div>
+
+<div className="preview" data-target="p-6">
+   <i className="fas fa-times"></i>
+   <img src="" alt=""></img>
+   <h3>Produit</h3>
+   <div className="stars">
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star-half-alt"></i>
+   </div>
+   <p>Details du produit</p>
+   <div className="price">Prix</div>
+   <div className="buttons">
+      <a href="#" className="buy">COMMANDEZ</a>
+      <a href="#" className="cart">Ajoutez au Panier</a>
+   </div>
+</div>
+
+</div>
+        </div>
+    );
+;
+    }
+export default crocs;
